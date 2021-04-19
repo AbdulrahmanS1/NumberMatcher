1a7b03eac8dbca39165ab35b7c84baa021166cff
AbdulrahmanS <Abdulrhman.Sarawiq@tuwaiq.edu.sa>
2021-04-19 11:36:34 +0300

    Add project files.

---
+++ NumberMatcher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace NumberMatcher
+{
+    class Program
+    {
+       static bool checkOrder(string source)
+        {
+            Stack openStack = new Stack();
+            int i = 0;
+            char temp;
+            while (i < source.Length)
+            {
+                if (openStack.Count == 0)
+                {
+                    openStack.Push(source[i]);
+                    i++;
+                    continue;
+                }
+
+                temp = (char)openStack.Peek();
+
+                if (source[i] == temp)
+                {
+                    openStack.Pop();
+                }
+                else if (source[i] != temp)
+                {
+                    openStack.Push(source[i]);
+                }
+                i++;
+            }
+
+            return openStack.Count == 0;
+        }
+
+        static void Main(string[] args)
+        {
+           Console.WriteLine(checkOrder("98766789"));
+        }
+    }
+}
+
